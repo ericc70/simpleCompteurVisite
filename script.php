0158c3f68e93e1469d74cc084b69195e1e969498
ericc70 <56532551+ericc70@users.noreply.github.com>
2020-09-17 14:31:46 +0200

    Update script.php

--- script.php
+++ script.php
@@ -8,20 +8,21 @@ if(file_exists($filename)){
   
 if(isset($visitors[$_SERVER['REMOTE_ADDR']]) ){
 
-$nb=$visitors[ $_SERVER['REMOTE_ADDR']  ] [1] ;
-$first=$visitors[ $_SERVER['REMOTE_ADDR']  ] [2] ;
-$nb++; 
-}
-else{$nb =1;
-$first = time();
-}
+ $nb=$visitors[ $_SERVER['REMOTE_ADDR']  ] [1] ;
+ $first=$visitors[ $_SERVER['REMOTE_ADDR']  ] [2] ;
+ $nb++; 
+ }
+ else{
+  $nb =1;
+  $first = time();
+ }
 
-$visitors[ $_SERVER['REMOTE_ADDR']]= array( 
-   time(),
-   $nb,
-   $first
-   );
+ $visitors[ $_SERVER['REMOTE_ADDR']]= array( 
+    time(),
+    $nb,
+    $first
+    );
 
 
-file_put_contents( $filename, serialize($visitors) );
+ file_put_contents( $filename, serialize($visitors) );
 }
