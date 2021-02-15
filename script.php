9c4681d47770fb3d6e738b22340e7f5af55ddcc0
ericc70 <56532551+ericc70@users.noreply.github.com>
2021-02-15 14:05:47 +0100

    add http_refferrer

--- script.php
+++ script.php
@@ -1,7 +1,7 @@
 <?php
 $filename = 'compteur.txt';
 $visitors = array();
- 
+$referent = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : 'no definied'; 
 if(file_exists($filename)){
     $visitors = unserialize( file_get_contents($filename) );
 
@@ -10,9 +10,18 @@ if(isset($visitors[$_SERVER['REMOTE_ADDR']]) ){
 
  $nb=$visitors[ $_SERVER['REMOTE_ADDR']  ] [1] ;
  $first=$visitors[ $_SERVER['REMOTE_ADDR']  ] [2] ;
+ $firstRef=$visitors[ $_SERVER['REMOTE_ADDR']  ] [3] ;
+
+ if (!in_array($referent, $firstRef ))
+ {
+      array_push($firstRef, $referent );
+  }
+
+ 
  $nb++; 
  }
  else{
+  $firstRef= [ $referent ];
   $nb =1;
   $first = time();
  }
@@ -20,7 +29,8 @@ if(isset($visitors[$_SERVER['REMOTE_ADDR']]) ){
  $visitors[ $_SERVER['REMOTE_ADDR']]= array( 
     time(),
     $nb,
-    $first
+    $first,
+   $firstRef
     );
 
 
